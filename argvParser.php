dfa8c9d9e64ea6d2a70e001af950b609b5d8c548
yuhang.chang <yuhang.chang@104.com.tw>
2020-03-26 13:16:52 +0800

    argv parse

--- argvParser.php
+++ argvParser.php
@@ -14,12 +14,30 @@ class argvParser
     private $argv;
     public function __construct($argv)
     {
+        $sortval = 'adult, child, sum, addr, init';
         $this->argv = $argv;
         $this->specs = new OptionCollection;
-        $this->specs->add('s|sort+', '排序方法-s=a')
-            ->isa('String');
-        $this->specs->add('f|fil+', '篩選方法')
-            ->isa('String');
+        $this->specs->add('s|sort+', '排序方法'.$sortval)
+            ->isa('string');
+        $this->specs->add('sortDecrease+', '排序方法'.$sortval)
+            ->isa('string');
+        $this->specs->add('sortIncrease+', '排序方法'.$sortval)
+            ->isa('string');
+
+        $this->specs->add('d|address+', '篩選地址')
+            ->isa('string');
+        $this->specs->add('i|institution+', '篩選機構名')
+            ->isa('string');
+        $this->specs->add('a|adult+', '用成人口罩數量篩選')
+            ->isa('Number');
+        $this->specs->add('c|child+', '用小孩口罩數量篩選')
+            ->isa('Number');
+        $this->specs->add('sum+', '用總數篩選')
+            ->isa('Number');
+        $this->specs->add('returnLimit:', '最大回傳數量')
+            ->isa('Number');
+        $this->specs->add('sendToTeams', '發送到teams');
+
         $this->specs->add('h|help', '列出可用選項');
     }
 
@@ -36,11 +54,15 @@ class argvParser
             $result = $parser->parse($this->argv);
             $re = [];
             foreach ($result->keys as $key => $spec) {
+                if ($key == 'help') {
+                    $this->getHelp();
+                }
                 $re[$key] = $spec->value;
             }
             return($re);
         } catch (Exception $e) {
             echo $e->getMessage();
+            return [];
         }
     }
 }
