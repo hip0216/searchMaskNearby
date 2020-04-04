98b60e2b4e8e5b120907204b77f165fec63dc154
RUSSELL LI-SHUO TSENG <russell57260620@gmail.com>
2020-04-04 10:01:28 +0800

    first workable version

---
+++ mask.php
@@ -0,0 +1,72 @@
+<?php
+require_once("vendor/autoload.php");
+require_once("argvParser.php");
+require_once("src/commandRecognize.php");
+require_once("src/renderColor.php");
+use JamesGordo\CSV\Parser;
+use League\CLImate\CLImate;
+
+function downloadFile()
+{
+    $maskDataUrl = "http://data.nhi.gov.tw/Datasets/Download.ashx?rid=A21030000I-D50001-001&l=https://data.nhi.gov.tw/resource/mask/maskdata.csv";
+    if (time() - filemtime("maskdata.csv") > 300) {
+        unlink("maskdata.csv");
+    }
+    if (is_file("maskdata.csv") === false) {
+        if (file_put_contents("maskdata.csv", file_get_contents($maskDataUrl))) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return true;
+    }
+}
+
+function convertHeader($datas) {
+    $map = [
+        '醫事機構代碼' => ID,
+        '醫事機構名稱' => INSTITUTION,
+        '醫事機構地址' => ADDRESS,
+        '醫事機構電話' => PHONENUMBER,
+        '成人口罩剩餘數' => ADULT,
+        '兒童口罩剩餘數' => CHILD,
+        '來源資料時間' => TIME,
+    ];
+
+    $ret = [];
+    foreach ($datas as $id => $data){
+        foreach ($data as $key => $val) {
+            $ret[$id][$map[$key]] = $val;
+        }
+    }
+    return $ret;
+}
+
+if (downloadFile() === false) {
+    print("下載檔案錯誤");
+    exit();
+}
+
+// parse argv parameter to $option
+$parser = new argvParser($argv);
+$option = $parser->getOption();
+print_r($option);
+
+// convert maskdata
+$datas = new Parser("maskdata.csv");
+$datas = convertHeader($datas->all());
+$cmdRcnz = new CommandRecognize($datas);
+$cmdRcnz->run($option);
+$datas = $cmdRcnz->getTable();
+
+// show
+$climate = new CLImate();
+if ($datas)
+    $climate->table($datas);
+else
+    echo "No Result\n";
+
+
+
+
